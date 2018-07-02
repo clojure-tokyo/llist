d4cdc305f51ffe077e3b92026827294a72ff7351
Curt J. Sampson <cjs@cynic.net>
2018-07-02 21:05:22 +0900

    Implement lfirst
    lfirst returns nil on an empty list, rather than throwing an error.
    This is likely to lead to programming errors, but it's what Clojure's
    first does, so we do it that way.
    
    Incidentally we reformatted the lrest line to match spacing of the
    new lfirst.

--- test/llist/core_test.clj
+++ test/llist/core_test.clj
@@ -5,17 +5,20 @@
 (deftest empty-list
   (let [l0 (lempty)]
     (is (= true (lempty? l0)))
-    (is (= 0 (llen l0)))))
+    (is (= 0 (llen l0)))
+    (is (= nil (lfirst l0)))))  ; !!! Well, Clojure's `first` does this!
 
 (deftest singleton
   (let [l1 (lcons 11 (lempty))]
     (is (= false (lempty? l1)))
-    (is (= 1 (llen l1)))))
+    (is (= 1 (llen l1)))
+    (is (= 11 (lfirst l1)))))
 
 (deftest doublet
   (let [l2 (lcons 22 (lcons 12 (lempty)))]
     (is (= false (lempty? l2)))
-    #_(is (= 2 (llen l2)))))
+    #_(is (= 2 (llen l2)))
+    (is (= 22 (lfirst l2)))))
 
 (deftest lempty-test
     (let [l0 (lempty)
